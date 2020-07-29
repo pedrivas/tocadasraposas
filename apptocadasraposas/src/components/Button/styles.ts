0ade2b6fdc173a842f74287015d914392765ea43
pedrivas <42252328+pedrivas@users.noreply.github.com>
2020-07-28 23:08:47 -0300

    formularios mobile com unform

---
+++ apptocadasraposas/src/components/Button/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled(RectButton)`
+  width: 100%;
+  height: 60px;
+  background: #F9DC5C;
+  border-radius: 10px;
+  margin-top: 8px;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: 'RobotoSLab-Medium';
+  color: #312e38;
+  font-size: 18px;
+`;
